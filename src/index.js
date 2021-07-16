de28afdd56ba7b7edba05928485c6590e5ed1cf2
Vinicius Reia Canalles <vicanalles@gmail.com>
2021-07-15 22:17:46 -0300

    Inserindo correções no código

--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
-const express = require("express");
+const express = require('express')
 
-const { v4: uuid } = require("uuid");
+const { v4: uuid } = require('uuid')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-const repositories = [];
+const repositories = []
 
-app.get("/repositories", (request, response) => {
-  return response.json(repositories);
-});
+function checkExistsRepository(request, response, next) {
+  const { id } = request.params
 
-app.post("/repositories", (request, response) => {
+  const repository = repositories.find(repository => repository.id === id)
+
+  if (!repository) {
+    return response.status(404).json({
+      error: 'Repository not found!'
+    })
+  }
+
+  request.repository = repository
+
+  return next()
+}
+
+function verifyFieldsToUpdate(repository, title, url, techs) {
+  if (title != '') {
+    repository.title = title
+  }
+
+  if (url != '') {
+    repository.url = url
+  }
+
+  if (techs.length != 0) {
+    repository.techs = techs
+  }
+
+  return repository
+}
+
+app.get('/repositories', (request, response) => {
+  return response.json(repositories)
+})
+
+app.post('/repositories', (request, response) => {
   const { title, url, techs } = request.body
 
   const repository = {
@@ -21,54 +53,59 @@ app.post("/repositories", (request, response) => {
     url,
     techs,
     likes: 0
-  };
-
-  return response.json(repository);
-});
-
-app.put("/repositories/:id", (request, response) => {
-  const { id } = request.params;
-  const updatedRepository = request.body;
-
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
-
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  //Comando para inserir repositório criado na lista de repositórios
+  repositories.push(repository)
 
-  repositories[repositoryIndex] = repository;
+  return response.status(201).json(repository)
+})
 
-  return response.json(repository);
-});
+app.put('/repositories/:id', checkExistsRepository, (request, response) => {
+  const { likes } = request.body
+  const { title, url, techs } = request.body
+  const { repository } = request
 
-app.delete("/repositories/:id", (request, response) => {
-  const { id } = request.params;
+  console.log('Likes: ' + likes)
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (likes === '' || likes === undefined) {
+    const updatedRepository = verifyFieldsToUpdate(
+      repository,
+      title,
+      url,
+      techs
+    )
 
-  if (repositoryIndex > 0) {
-    return response.status(404).json({ error: "Repository not found" });
+    return response.status(200).json(updatedRepository)
+  } else {
+    return response.status(200).json({
+      likes: 0
+    })
   }
+})
 
-  repositories.splice(repositoryIndex, 1);
+app.delete('/repositories/:id', checkExistsRepository, (request, response) => {
+  const { repository } = request
 
-  return response.status(204).send();
-});
+  const repositoryIndex = repositories.indexOf(repository)
 
-app.post("/repositories/:id/like", (request, response) => {
-  const { id } = request.params;
+  repositories.splice(repositoryIndex, 1)
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  return response.status(204).send()
+})
 
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
+app.post(
+  '/repositories/:id/like',
+  checkExistsRepository,
+  (request, response) => {
+    const { repository } = request
+
+    repository.likes += 1
+
+    return response.status(200).json({
+      likes: repository.likes
+    })
   }
+)
 
-  const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
-});
-
-module.exports = app;
+module.exports = app
